60e1911ec58635dfb708a2ff2976430f1f1e2114
philornot <philornot@users.noreply.github.com>
2025-10-03 22:10:08 +0200

    TK-313: Interceptor do dodawania Authorization header

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,4 +12,18 @@ const api = axios.create({
   },
 });
 
+// Request interceptor - dodaj JWT token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
